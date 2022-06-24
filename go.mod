5269d246a3f3683f82bb8c8a80039a678ec05e57
burntcarrot <aadhav.n1@gmail.com>
2022-06-24 10:01:59 +0530

    tests: add unit tests
    Signed-off-by: burntcarrot <aadhav.n1@gmail.com>

--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module github.com/deepsourcelabs/deepsource-go
 
 go 1.17
 
-require github.com/pelletier/go-toml/v2 v2.0.2
+require (
+	github.com/dave/jennifer v1.5.0
+	github.com/go-test/deep v1.0.8
+	github.com/pelletier/go-toml/v2 v2.0.2
+)
 
 require github.com/davecgh/go-spew v1.1.1 // indirect
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,14 +2,18 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
 	"os"
+	"strings"
 	"testing"
+
+	"github.com/go-test/deep"
 )
 
 func TestWriteTOML(t *testing.T) {
 	var testBuf bytes.Buffer
 
-	normalTOML, err := os.ReadFile("testdata/U001.toml")
+	normalTOML, err := os.ReadFile("testdata/src/issues/U001.toml")
 	if err != nil {
 		t.Error("failed to read testdata.")
 	}
@@ -39,4 +43,82 @@ func TestWriteTOML(t *testing.T) {
 			t.Errorf("description: %s, got: %v, want: %v\n", tc.description, got, tc.want)
 		}
 	}
+}
+
+func TestTraverseAST(t *testing.T) {
+
+}
+
+func TestCodeGenerator(t *testing.T) {
+	exampleContent, err := os.ReadFile("testdata/src/codegen/example.go")
+	if err != nil {
+		t.Error("failed to read testdata.")
+	}
+
+	type test struct {
+		description       string
+		pluginAnalyzerMap map[string][]string
+		want              string
+	}
+
+	tests := []test{
+		{description: "some plugins", pluginAnalyzerMap: map[string][]string{
+			"go-ast": {"hello", "hi"},
+		}, want: string(exampleContent)},
+	}
+
+	for _, tc := range tests {
+		f := codeGenerator(tc.pluginAnalyzerMap)
+		got := fmt.Sprintf("%#v", f)
+
+		diffs, result := checkEquality(got, tc.want)
+
+		if !result {
+			t.Errorf("description: %s, content doesn't match\n", tc.description)
+			t.Log("differences in diffs:", diffs)
+		}
+	}
+}
+
+func TestWalkDir(t *testing.T) {
+	type test struct {
+		description string
+		directory   string
+		want        []string
+	}
+
+	tests := []test{
+		{description: "walk testdata", directory: "testdata/src/", want: []string{"testdata/src/codegen/example.go", "testdata/src/issues/U001.toml"}},
+	}
+
+	for _, tc := range tests {
+		got, err := walkDir(tc.directory)
+		if err != nil {
+			t.Error(err)
+		}
+
+		diffs := deep.Equal(got, tc.want)
+		if len(diffs) != 0 {
+			t.Errorf("description: %s, file names don't match\n", tc.description)
+			t.Log("differences in diffs:", diffs)
+		}
+	}
+}
+
+// checkEquality is a helper for checking string differences. Handles indentation differences, etc.
+func checkEquality(got, want string) ([]string, bool) {
+	var gotLines []string
+	for _, line := range strings.Split(got, "\n") {
+		trimmed := strings.TrimSpace(line)
+		gotLines = append(gotLines, trimmed)
+	}
+
+	var wantLines []string
+	for _, line := range strings.Split(want, "\n") {
+		trimmed := strings.TrimSpace(line)
+		wantLines = append(wantLines, trimmed)
+	}
+
+	diff := deep.Equal(gotLines, wantLines)
+	return diff, len(diff) == 0
 }
